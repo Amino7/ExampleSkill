b0f1ed5fac51a3f88643f3030be4c1be8c2878c8
Amin U <amin.uzdenov@hotmail.de>
2018-09-10 10:02:28 +0200

    basic setup

---
+++ app.js
@@ -0,0 +1,52 @@
+const Alexa = require('ask-sdk');
+let skill;
+
+if (ENVIRONMENT === 'production') {
+
+  exports.handler = async function (event, context) {
+    if (!skill) {
+      skill = Alexa.SkillBuilders.custom()
+        .addRequestHandlers(
+          HelloWorldHandler
+        )
+        .create();
+    }
+    return skill.invoke(event,context);
+  }
+
+} else {
+
+  // Development environment - we are on our local node server
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.post('/', function(req, res) {
+
+    if (!skill) {
+
+      skill = Alexa.SkillBuilders.custom()
+        .addRequestHandlers(
+          HelloWorldHandler
+        )
+        .create();
+
+    }
+
+    skill.invoke(req.body)
+      .then(function(responseBody) {
+        res.json(responseBody);
+      })
+      .catch(function(error) {
+        console.log(error);
+        res.status(500).send('Error during the request');
+      });
+
+  });
+
+  app.listen(3000, function () {
+    console.log('Development endpoint listening on port 3000!');
+  });
+
+}
